2ce0530164b707a9e65bdc9cdb02fe60de607c4e
Alexis Gilberth Renteria <gilberthr10@gmail.com>
2022-09-04 22:56:34 -0500

    ADD: empty state in favoritesView, add letter spacing in index.css

--- src/modules/pokemon/views/FavoritesView.vue
+++ src/modules/pokemon/views/FavoritesView.vue
@@ -8,6 +8,14 @@
         :pokemon="pokemon"
       />
     </div>
+    <!-- Empty state for favorites -->
+    <div v-if="!pokemonsFavorites.length" class="empty-state">
+      <img
+        src="https://i.pinimg.com/originals/1f/0b/85/1f0b85bb750807778b1fe2444527fbd2.gif"
+        alt="Not found page"
+      />
+      <h3>Tu pokedex esta vacio</h3>
+    </div>
   </div>
 </template>
 
@@ -29,6 +37,24 @@ const { pokemonsFavorites } = usePokemon();
   margin: 0 auto;
   max-width: 100%;
   gap: 1rem;
+}
+.empty-state {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  max-width: 100%;
+  margin: 0 auto;
+  width: 400px;
+}
+img {
+  width: 100%;
+  max-width: 350px;
+}
+h3 {
+  font-size: 2rem;
+  margin: 0;
+  font-weight: 100;
 }
 /* media query*/
 @media (max-width: 700px) {
